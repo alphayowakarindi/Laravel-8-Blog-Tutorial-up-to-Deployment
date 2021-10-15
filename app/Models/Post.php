ccbc01e3e67f85c5b9054b4a24c9c12540003d99
Alphy Gacheru <alphygacheru@gmail.com>
2021-10-16 00:46:26 +0300

    #23 Eloquent Relationship between a post and user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // We have post and it belongsTo a user
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // We have a user and a user hasMany posts
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 }
