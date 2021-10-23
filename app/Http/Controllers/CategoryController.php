ac87f4daf1a60896812d7ee17f42a16cdc4ca20b
Alphy Gacheru <alphygacheru@gmail.com>
2021-10-23 06:13:41 +0300

    #39 Category Create Page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -24,7 +24,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.create-category');
     }
 
     /**
---
+++ resources/views/categories/create-category.blade.php
@@ -0,0 +1,39 @@
+@extends('layout')
+@section('head')
+    <script src="https://cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
+@endsection
+@section('main')
+    <main class="container" style="background-color: #fff;">
+        <section id="contact-us">
+            <h1 style="padding-top: 50px;">Create New Category!</h1>
+            @if (session('status'))
+                <p
+                    style="color: #fff; width:100%;font-size:18px;font-weight:600;text-align:center;background:#5cb85c;padding:17px 0;margin-bottom:6px;">
+                    {{ session('status') }}</p>
+            @endif
+
+            <!-- Contact Form -->
+            <div class="contact-form">
+                <form action="{{ route('categories.store') }}" method="post" >
+                    @csrf
+                    <!-- name -->
+                    <label for="name"><span>Name</span></label>
+                    <input type="text" id="name" name="name" value="{{ old('name') }}" />
+                    @error('name')
+                        {{-- The $attributeValue field is/must be $validationRule --}}
+                        <p style="color: red; margin-bottom:25px;">{{ $message }}</p>
+                    @enderror
+                
+                    <!-- Button -->
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </section>
+    </main>
+@endsection
+
+@section('scripts')
+    <script>
+        CKEDITOR.replace('body');
+    </script>
+@endsection
